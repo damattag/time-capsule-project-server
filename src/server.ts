2047bb399d56058a110aec4503844a1ba32d5c91
damattag <guilherme.damatta@yahoo.com.br>
2023-05-19 01:18:57 -0300

    create upload routes

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,21 @@ import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import Static from "@fastify/static";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
+
+app.register(multipart);
+
+app.register(Static, {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true,
@@ -17,6 +28,7 @@ app.register(jwt, {
 });
 
 app.register(authRoutes, { prefix: "/register" });
+app.register(uploadRoutes, { prefix: "/upload" });
 app.register(memoriesRoutes, { prefix: "/memories" });
 
 app
